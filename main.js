d2ff77dfa8434efcd4b5b896409e1aaa45818515
acl13 <128934431+acl13@users.noreply.github.com>
2024-08-06 15:10:47 -0400

    refactor to use templates and render HTML dynamically

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ document.getElementById("search").addEventListener("click", function () {
     fetchWeatherData(userSearch);
   }
 
-  userSearch.textContent = "";
+  document.getElementById("search-query").value = "";
 });
 
 const fetchWeatherData = (city) => {
@@ -33,63 +33,38 @@ const fetchWeatherData = (city) => {
 fetchWeatherData("Charlotte");
 
 const displayCurrentWeather = (data) => {
+  document.querySelector(".current-weather").replaceChildren();
   const fahrenheitTemp = convertKelvinToFahrenheit(data.list[0].main.temp);
   const iconURL = `https://openweathermap.org/img/wn/${data.list[0].weather[0].icon}@2x.png`;
 
-  document.getElementById("current-temp").textContent =
-    `${fahrenheitTemp}\u00B0`;
-  document.getElementById("city").textContent = data.city.name;
-  document.getElementById("current-condition").textContent =
-    data.list[0].weather[0].main;
-  document.getElementById("current-icon").src = iconURL;
+  const template = ` 
+    <div class="col-md-6 justify-center">
+      <h2>${fahrenheitTemp}&deg</h2>
+      <h3 id="city">${data.city.name}</h3>
+      <h4 id="current-condition">${data.list[0].weather[0].main}</h4>
+    </div>
+    <div class="col-md-6">
+      <img
+        src= "${iconURL}"
+        />
+      </div>`;
+
+  document
+    .querySelector(".current-weather")
+    .insertAdjacentHTML("beforeend", template);
 };
 
 const displayForecast = (data) => {
-  console.log(data);
+  document.querySelector(".forecast").replaceChildren();
   const dayOne = data.list[7];
   const dayTwo = data.list[15];
   const dayThree = data.list[23];
   const dayFour = data.list[31];
   const dayFive = data.list[39];
+  const forecastDisplay = [dayOne, dayTwo, dayThree, dayFour, dayFive];
 
-  document.getElementById("day-one-condition").textContent =
-    dayOne.weather[0].main;
-  document.getElementById("day-one-temp").textContent =
-    `${convertKelvinToFahrenheit(dayOne.main.temp)}\u00B0`;
-  document.getElementById("day-one-icon").src =
-    `https://openweathermap.org/img/wn/${dayOne.weather[0].icon}@2x.png`;
-
-  document.getElementById("day-two-condition").textContent =
-    dayTwo.weather[0].main;
-  document.getElementById("day-two-temp").textContent =
-    `${convertKelvinToFahrenheit(dayTwo.main.temp)}\u00B0`;
-  document.getElementById("day-two-icon").src =
-    `https://openweathermap.org/img/wn/${dayTwo.weather[0].icon}@2x.png`;
-
-  document.getElementById("day-three-condition").textContent =
-    dayThree.weather[0].main;
-  document.getElementById("day-three-temp").textContent =
-    `${convertKelvinToFahrenheit(dayThree.main.temp)}\u00B0`;
-  document.getElementById("day-three-icon").src =
-    `https://openweathermap.org/img/wn/${dayThree.weather[0].icon}@2x.png`;
-
-  document.getElementById("day-four-condition").textContent =
-    dayFour.weather[0].main;
-  document.getElementById("day-four-temp").textContent =
-    `${convertKelvinToFahrenheit(dayFour.main.temp)}\u00B0`;
-  document.getElementById("day-four-icon").src =
-    `https://openweathermap.org/img/wn/${dayFour.weather[0].icon}@2x.png`;
-
-  document.getElementById("day-five-condition").textContent =
-    dayFive.weather[0].main;
-  document.getElementById("day-five-temp").textContent =
-    `${convertKelvinToFahrenheit(dayFive.main.temp)}\u00B0`;
-  document.getElementById("day-five-icon").src =
-    `https://openweathermap.org/img/wn/${dayFive.weather[0].icon}@2x.png`;
-};
-
-const displayDays = () => {
   // Weekdays listed twice to account for overlap
+  const date = new Date();
   const days = [
     "Sunday",
     "Monday",
@@ -106,18 +81,31 @@ const displayDays = () => {
     "Friday",
     "Saturday",
   ];
-  const date = new Date();
-  document.getElementById("day-one").textContent = days[date.getDay() + 1];
-  document.getElementById("day-two").textContent = days[date.getDay() + 2];
-  document.getElementById("day-three").textContent = days[date.getDay() + 3];
-  document.getElementById("day-four").textContent = days[date.getDay() + 4];
-  document.getElementById("day-five").textContent = days[date.getDay() + 5];
+
+  forecastDisplay.forEach((day) => {
+    const index = forecastDisplay.indexOf(day);
+    const forecastIconURL = `https://openweathermap.org/img/wn/${day.weather[0].icon}@2x.png`;
+    const forecastTemplate = `
+    <div class="col-md-2 justify-center p-5 border" id="${index}">
+          <h5 id="day-one-condition">${day.weather[0].main}</h5>
+          <h5 id="day-one-temp">${convertKelvinToFahrenheit(day.main.temp)}&deg</h5>
+          <img
+            id="day-one-icon"
+            src="${forecastIconURL}"
+          />
+          <h6 id="day-one">${days[date.getDay() + (index + 1)]}</h6>
+        </div>`;
+
+    document
+      .querySelector(".forecast")
+      .insertAdjacentHTML("beforeend", forecastTemplate);
+  });
+  document.getElementById(`${0}`).classList.add("offset-md-1");
 };
 
 const displayWeatherData = (data) => {
   displayCurrentWeather(data);
   displayForecast(data);
-  displayDays();
 };
 
 const convertKelvinToFahrenheit = (deg) => {
