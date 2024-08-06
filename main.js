22e83c6e0f916585872d00dd9425c8c832ae66c0
acl13 <128934431+acl13@users.noreply.github.com>
2024-08-06 11:39:01 -0400

    display 5 day forecast data

--- main.js
+++ main.js
@@ -6,13 +6,12 @@ const fetchWeatherData = (city) => {
     dataType: "json",
   })
     .then((data) => data.json())
-    .then((data) => displayCurrentWeather(data));
+    .then((data) => displayWeatherData(data));
 };
 
 fetchWeatherData("Charlotte");
 
 const displayCurrentWeather = (data) => {
-  console.log(data);
   const fahrenheitTemp = convertKelvinToFahrenheit(data.list[0].main.temp);
   const iconURL = `https://openweathermap.org/img/wn/${data.list[0].weather[0].icon}@2x.png`;
 
@@ -24,7 +23,54 @@ const displayCurrentWeather = (data) => {
   document.getElementById("current-icon").src = iconURL;
 };
 
-const displayForecast = (data) => {};
+const displayForecast = (data) => {
+  console.log(data);
+  const dayOne = data.list[7];
+  const dayTwo = data.list[15];
+  const dayThree = data.list[23];
+  const dayFour = data.list[31];
+  const dayFive = data.list[39];
+
+  document.getElementById("day-one-condition").textContent =
+    dayOne.weather[0].main;
+  document.getElementById("day-one-temp").textContent =
+    `${convertKelvinToFahrenheit(dayOne.main.temp)}\u00B0`;
+  document.getElementById("day-one-icon").src =
+    `https://openweathermap.org/img/wn/${dayOne.weather[0].icon}@2x.png`;
+
+  document.getElementById("day-two-condition").textContent =
+    dayTwo.weather[0].main;
+  document.getElementById("day-two-temp").textContent =
+    `${convertKelvinToFahrenheit(dayTwo.main.temp)}\u00B0`;
+  document.getElementById("day-two-icon").src =
+    `https://openweathermap.org/img/wn/${dayTwo.weather[0].icon}@2x.png`;
+
+  document.getElementById("day-three-condition").textContent =
+    dayThree.weather[0].main;
+  document.getElementById("day-three-temp").textContent =
+    `${convertKelvinToFahrenheit(dayThree.main.temp)}\u00B0`;
+  document.getElementById("day-three-icon").src =
+    `https://openweathermap.org/img/wn/${dayThree.weather[0].icon}@2x.png`;
+
+  document.getElementById("day-four-condition").textContent =
+    dayFour.weather[0].main;
+  document.getElementById("day-four-temp").textContent =
+    `${convertKelvinToFahrenheit(dayFour.main.temp)}\u00B0`;
+  document.getElementById("day-four-icon").src =
+    `https://openweathermap.org/img/wn/${dayFour.weather[0].icon}@2x.png`;
+
+  document.getElementById("day-five-condition").textContent =
+    dayFive.weather[0].main;
+  document.getElementById("day-five-temp").textContent =
+    `${convertKelvinToFahrenheit(dayFive.main.temp)}\u00B0`;
+  document.getElementById("day-five-icon").src =
+    `https://openweathermap.org/img/wn/${dayFive.weather[0].icon}@2x.png`;
+};
+
+const displayWeatherData = (data) => {
+  displayCurrentWeather(data);
+  displayForecast(data);
+};
 
 const convertKelvinToFahrenheit = (deg) => {
   return Math.round((deg - 273) * 1.8 + 32);
