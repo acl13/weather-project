2ded236352c0acedbd9ee80e5fd4c08c93178ba1
acl13 <128934431+acl13@users.noreply.github.com>
2024-08-14 11:07:35 -0400

    fix comments from pull request

--- main.js
+++ main.js
@@ -1,8 +1,29 @@
-document.getElementById("search").addEventListener("click", function () {
-  const userSearch = document.getElementById("search-query").value;
-  console.log(userSearch);
+const searchButton = document.getElementById("search");
+// Weekdays listed twice to account for overlap
+const days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
 
-  if (userSearch === "") {
+searchButton.addEventListener("click", function () {
+  const userSearch = document.getElementById("search-query").value;
+
+  if (
+    !document.getElementById("search-query").checkValidity() ||
+    userSearch.trim().length === 0
+  ) {
     alert("Please enter the name of a city");
   } else {
     fetchWeatherData(userSearch);
@@ -11,12 +32,22 @@ document.getElementById("search").addEventListener("click", function () {
   document.getElementById("search-query").value = "";
 });
 
-const fetchWeatherData = (city) => {
-  const url = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=a01dd8f2b7b0fd48756b05d7cb1e2fe5`;
-  const searchButton = document.getElementById("search");
+const searchLoading = () => {
   searchButton.disabled = true;
   searchButton.innerHTML = `<i class="fa fa-spinner fa-spin"></i>Loading
 `;
+};
+
+const searchDone = () => {
+  searchButton.disabled = false;
+  searchButton.innerHTML = "Search";
+};
+
+const fetchWeatherData = (city) => {
+  const url = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=a01dd8f2b7b0fd48756b05d7cb1e2fe5`;
+
+  searchLoading();
+
   fetch(url, {
     method: "GET",
     dataType: "json",
@@ -24,13 +55,17 @@ const fetchWeatherData = (city) => {
     .then((data) => data.json())
     .then((data) => {
       displayWeatherData(data);
-      searchButton.disabled = false;
-      searchButton.innerHTML = "Search";
+      searchDone();
     })
     .catch(handleError);
 };
 
 fetchWeatherData("Charlotte");
+
+const displayWeatherData = (data) => {
+  displayCurrentWeather(data);
+  displayForecast(data);
+};
 
 const displayCurrentWeather = (data) => {
   document.querySelector(".current-weather").replaceChildren();
@@ -56,34 +91,15 @@ const displayCurrentWeather = (data) => {
 
 const displayForecast = (data) => {
   document.querySelector(".forecast").replaceChildren();
-  const dayOne = data.list[7];
-  const dayTwo = data.list[15];
-  const dayThree = data.list[23];
-  const dayFour = data.list[31];
-  const dayFive = data.list[39];
-  const forecastDisplay = [dayOne, dayTwo, dayThree, dayFour, dayFive];
 
-  // Weekdays listed twice to account for overlap
+  // Forecast data is stored in three hour intervals. This fetches the data every 8 intervals, or 24 hours
+  const dailyIntervals = [7, 15, 23, 31, 39];
+  const fiveDayForecast = dailyIntervals.map((interval) => data.list[interval]);
+
   const date = new Date();
-  const days = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-  ];
 
-  forecastDisplay.forEach((day) => {
-    const index = forecastDisplay.indexOf(day);
+  fiveDayForecast.forEach((day) => {
+    const index = fiveDayForecast.indexOf(day);
     const forecastIconURL = `https://openweathermap.org/img/wn/${day.weather[0].icon}@2x.png`;
     const forecastTemplate = `
     <div class="col-md-2 justify-center p-5 border" id="${index}">
@@ -103,15 +119,21 @@ const displayForecast = (data) => {
   document.getElementById(`${0}`).classList.add("offset-md-1");
 };
 
-const displayWeatherData = (data) => {
-  displayCurrentWeather(data);
-  displayForecast(data);
-};
-
 const convertKelvinToFahrenheit = (deg) => {
   return Math.round((deg - 273) * 1.8 + 32);
 };
 
 function handleError(error) {
-  console.log(`ERROR: ${error}`);
+  if (error instanceof TypeError) {
+    alert(`Oops. Something went wrong.
+      Please check that:
+       - You have entered a valid city 
+       - You are connected to the interent`);
+  } else {
+    alert(
+      `Oops. Looks like something went wrong on our end. 
+      ERROR: ${error}`
+    );
+  }
+  searchDone();
 }
